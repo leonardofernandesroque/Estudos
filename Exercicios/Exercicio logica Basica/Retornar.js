a1da33feebc476f429937ac498567bd2d7b9d4bc
leonardo fernandes roque <53011600+leonardofernandesroque@users.noreply.github.com>
2021-08-26 02:33:45 -0300

    Add files via upload

---
+++ Exercicios/Exercicio logica Basica/Retornar.js
@@ -0,0 +1,9 @@
+
+function max(x,y){  
+    if(x > y) return x;
+    return y;
+}
+
+//const max2 = (x,y) => x > y ? x : y;
+
+console.log(max(10,2));
---
+++ Exercicios/Exercicio logica Basica/paisagem.js
@@ -0,0 +1,9 @@
+
+//function ePaisagem(largura, altura){
+//    return largura > altura ? true : false;
+//}
+
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080));
